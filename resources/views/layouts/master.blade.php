55b5d423250b745adc3464a1848bb33f0a98b330
3A532089 <likebestupid@gmail.com>
2018-11-09 23:42:08 +0800

    建立主樣板

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>@yield('title')| My Blog</title>
+</head>
+<body>
+    <div class="container">
+       @yield('content')
+    </div>    
+</body>
+</html>
