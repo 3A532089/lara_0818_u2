f9de9ef8214c8283c631f90819f1a7c11aba6f35
3A532089 <likebestupid@gmail.com>
2018-11-09 22:42:26 +0800

    使用Controller回傳

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,3 +11,11 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('home.index');
+    }
+}
